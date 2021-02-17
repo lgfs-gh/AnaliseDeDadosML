7ccd773b7c39af613d1930c48efdf5c691647870
lestat <luiiz@live.com>
2021-02-16 22:32:51 -0300

    Versao Teste 0.2 - DFGEN por intervalos

--- gerador.py
+++ gerador.py
@@ -26,26 +26,27 @@ class GenDataFrame:
     mes -> o usuário define com um número (int) o mês desejado.
     ano -> o usuário define com um número (int) o ano desejado.
 
-    Por padrão os valores dos atributos são iniciados com a string 'todos'
+    Por padrão os valores dos atributos são iniciados como None
     assim retornado todos os valores do dataframe base.
     Alterando os parâmetros de criação, é possível obter por exemplo:
 
-    **Para gerar o dataframe, é usado o método 'gerar()'
-
     1) Um dataframe contendo informações sobre a fonte eolica:
-    variavel = GenDataFrame(fonte='Eolicas').gerar()
+    variavel = GenDataFrame(fonte=['Eolicas']).gerar()
 
-    1) Um dataframe contendo informações sobre a fonte carvão no mês de janeiro, em todos os anos:
-    variavel = GenDataFrame(fonte='Carvao', mes=1).gerar()
+    1) Um dataframe contendo informações sobre a fonte carvão e eólica no mês de janeiro, em todos os anos:
+    variavel = GenDataFrame(fonte='[Carvao', 'Eolicas'], mes=[1]).gerar()
 
-    3) Um dataframe contendo informações sobre a fonte eolica em 2012:
-    variavel = GenDataFrame(fonte='Eolicas', ano=2012).gerar()
+    3) Um dataframe contendo informações sobre a fonte eolica em 2012, 2013 e 2014:
+    variavel = GenDataFrame(fonte=['Eolicas'], ano=[2012, 2013, 2014]).gerar()
 
-    4) Um dataframe contendo informações sobre todas as fontes em novembro de 2015:
-    variavel = GenDataFrame(mes=11, ano=2015).gerar()
+    4) Um dataframe contendo informações sobre todas as fontes em novembro de 2015 e 2016:
+    variavel = GenDataFrame(mes=[11], ano=[2015, 2016]).gerar()
+
+    **Para gerar o dataframe, é usado o método 'gerar()'
+    **Os parametros são passados dentro de container
     """
 
-    def __init__(self, fonte='todos', mes: int = 'todos', ano: int = 'todos'):
+    def __init__(self, fonte=None, mes=None, ano=None):
         self.__fonte = fonte
         self.__mes = mes
         self.__ano = ano
@@ -60,74 +61,75 @@ class GenDataFrame:
             dados['mes'].append(i[2])
             dados['ano'].append(i[3])
 
-        if self.__fonte == 'todos' and self.__mes == 'todos' and self.__ano == 'todos':
+        if self.__fonte is None and self.__mes is None and self.__ano is None:
             for i in self.__db:
                 preencher(parametros, i)
             df = pd.DataFrame(parametros)
             return df
-        elif self.__fonte != 'todos' and self.__mes != 'todos' and self.__ano != 'todos':
+        elif self.__fonte is not None and self.__mes is not None and self.__ano is not None:
             for i in self.__db:
-                if i[0] == self.__fonte and i[2] == self.__mes and i[3] == self.__ano:
+                if i[0] in self.__fonte and i[2] in self.__mes and i[3] in self.__ano:
                     preencher(parametros, i)
             df = pd.DataFrame(parametros)
             return df
-        elif self.__fonte != 'todos':
-            if self.__mes == 'todos' and self.__ano == 'todos':
+        elif self.__fonte is not None:
+            if self.__mes is None and self.__ano is None:
                 for i in self.__db:
-                    if i[0] == self.__fonte:
+                    if i[0] in self.__fonte:
                         preencher(parametros, i)
                 df = pd.DataFrame(parametros)
                 return df
-            elif self.__mes != 'todos' and self.__ano == 'todos':
+            elif self.__mes is not None and self.__ano is None:
                 for i in self.__db:
-                    if i[0] == self.__fonte and i[2] == self.__mes:
-                        preencher(parametros, i)
-                df = pd.DataFrame(parametros)
-                return df
-            else:
-                for i in self.__db:
-                    if i[0] == self.__fonte and i[3] == self.__ano:
-                        preencher(parametros, i)
-                df = pd.DataFrame(parametros)
-                return df
-        elif self.__mes != 'todos':
-            if self.__fonte == 'todos' and self.__ano == 'todos':
-                for i in self.__db:
-                    if i[2] == self.__mes:
-                        preencher(parametros, i)
-                df = pd.DataFrame(parametros)
-                return df
-            elif self.__fonte != 'todos' and self.__ano == 'todos':
-                for i in self.__db:
-                    if i[0] == self.__fonte and i[2] == self.__mes:
+                    if i[0] in self.__fonte and i[2] in self.__mes:
                         preencher(parametros, i)
                 df = pd.DataFrame(parametros)
                 return df
             else:
                 for i in self.__db:
-                    if i[2] == self.__mes and i[3] == self.__ano:
+                    if i[0] in self.__fonte and i[3] in self.__ano:
                         preencher(parametros, i)
                 df = pd.DataFrame(parametros)
                 return df
-        elif self.__ano != 'todos':
-            if self.__fonte == 'todos' and self.__mes == 'todos':
+        elif self.__mes is not None:
+            if self.__fonte is None and self.__ano is None:
                 for i in self.__db:
-                    if i[3] == self.__ano:
+                    if i[2] in self.__mes:
                         preencher(parametros, i)
                 df = pd.DataFrame(parametros)
                 return df
-            elif self.__fonte != 'todos' and self.__mes == 'todos':
+            elif self.__fonte is not None and self.__ano is None:
                 for i in self.__db:
-                    if i[0] == self.__fonte and i[3] == self.__ano:
+                    if i[0] in self.__fonte and i[2] in self.__mes:
                         preencher(parametros, i)
                 df = pd.DataFrame(parametros)
                 return df
             else:
                 for i in self.__db:
-                    if i[2] == self.__mes and i[3] == self.__ano:
+                    if i[2] in self.__mes and i[3] in self.__ano:
                         preencher(parametros, i)
                 df = pd.DataFrame(parametros)
                 return df
         else:
-            print('Algo deu errado')
+            if self.__fonte is None and self.__mes is None:
+                for i in self.__db:
+                    if i[3] in self.__ano:
+                        preencher(parametros, i)
+                df = pd.DataFrame(parametros)
+                return df
+            elif self.__fonte is not None and self.__mes is None:
+                for i in self.__db:
+                    if i[0] in self.__fonte and i[3] in self.__ano:
+                        preencher(parametros, i)
+                df = pd.DataFrame(parametros)
+                return df
+            else:
+                for i in self.__db:
+                    if i[2] in self.__mes and i[3] in self.__ano:
+                        preencher(parametros, i)
+                df = pd.DataFrame(parametros)
+                return df
 
+
+x = GenDataFrame(fonte=['Carvao', 'Eolicas'], mes=[1, 2, 3, 4, 5], ano=[2010, 2012, 2013]).gerar()
+print(x)
