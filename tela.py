488bfbb75ff4530ed4c5c335838791765845cb65
lgfs-gh <luiiz@live.com>
2021-02-19 17:38:36 -0300

    Versao Teste 0.1 - GUI

---
+++ tela.py
@@ -0,0 +1,28 @@
+import PySimpleGUI as sg
+
+
+class Interface:
+
+    def __init__(self):
+
+        # LAYOUT
+        layout = [
+            [sg.Text('Fonte(s)'), sg.InputText()],
+            [sg.Text('Mes(es)'), sg.InputText()],
+            [sg.Text('Ano(s)'), sg.Input()],
+            [sg.Button('Gerar')]
+        ]
+
+        # JANELA
+        janela = sg.Window('Dados do Dataframe').layout(layout)
+
+        # Extrair dados da tela
+        self.button, self.values = janela.Read()
+
+    def Iniciar(self):
+        for chave, valor in self.values.items():
+            print(valor)
+
+
+tela = Interface()
+tela.Iniciar()
